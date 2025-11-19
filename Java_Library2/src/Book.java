f8615b1f11e25752352e12fe5120dd5413f6c9fc
hyckju <hyckjoo0125@gmail.com>
2025-11-19 22:08:26 +0900

    도서 저장과 검색 기능 개발

--- Java_Library2/src/Book.java
+++ Java_Library2/src/Book.java
@@ -1,8 +1,39 @@
-public class Book {
-    private String title;
-    private String author;
-    private String publisher;
-    private String isbn;
+public class BookSaver {
+    private final String title;
+    private final String author;
+    private final String publisher;
+    private final String isbn;
+    private final String callNumber;
+
+    public BookSaver(String title, String author, String publisher, String isbn, String callNumber) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.callNumber = callNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public String getISBN() {
+        return isbn;
+    }
+    public String getCallNumber() {
+        return callNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "[도서명] " + title + "| [저자] " + author +  "| [출판사] "  + publisher +
+                "| [ISBN]"+ isbn + "| [청구 기호] " +  callNumber;
+    }
 
 
 }
