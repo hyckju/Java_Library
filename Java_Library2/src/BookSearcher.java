285021a29189811547e15a6a9b6179d6e14d7306
Prue <prue0115@gmail.com>
2025-11-21 19:17:45 +0900

    학과별 도서 추천

--- Java_Library2/src/BookSearcher.java
+++ Java_Library2/src/BookSearcher.java
@@ -15,7 +15,7 @@ public class BookSearcher {
         if (title == null || title.trim().isEmpty()) return result;
 
         for (Book book : storage.getBooks()) {
-            if (book.getTitle().equalsIgnoreCase(title)) {
+            if (book.getTitle().toLowerCase().contains(title.toLowerCase())) {
                 result.add(book);
             }
         }
@@ -27,14 +27,16 @@ public class BookSearcher {
 
         String targetCategory = switch (college) {
             case "공과대학" -> "기술과학";
-            case "인문사회대학" -> "철학";
-            case "자연과학대학" -> "과학";
+            case "인문사회대학" -> "철학";            // 단과대학 : 경상대학, 인문사회대학, 공과대학, 자연과학대학, 예쑬대학, 보건의료과학대학, AI·SW융합대학
+            case "자연과학대학" -> "자연";            // 카테고리 : 철학, 종교, 사회과학, 자연과학, 기술과학, 예술, 어학, 문학, 역사
             case "예술대학" -> "예술";
             case "경상대학" -> "경영";
             case "보건의료과학대학" -> "의학";
             case "AI·SW융합대학" -> "기술과학";
-            default -> ""; // 선택 안 했을 경우
+            default -> "";
         };
+
+        if (targetCategory.isEmpty()) return result;
 
         for (Book book : storage.getBooks()) {
             if (book.getCategory().contains(targetCategory)) {
--- Java_Library2/src/BookStorage.java
+++ Java_Library2/src/BookStorage.java
@@ -14,15 +14,14 @@ public class BookStorage {
     public List<Book> getBooks() {
         return books;
     }
-
-    // 샘플 도서
+    // 단과대학 : 경상대학, 인문사회대학, 공과대학, 자연과학대학, 예쑬대학, 보건의료과학대학, AI·SW융합대학
+    // 카테고리 : 철학, 종교, 사회과학, 자연과학, 기술과학, 예술, 어학, 문학, 역사
     public void sampleBooks() {
         books.add(new Book("데이터 통신", "Forouzan", "한빛아카데미", "004.5", "기술과학"));
         books.add(new Book("컴퓨터 네트워크", "김봉한", "한빛미디어", "004.6", "기술과학"));
         books.add(new Book("운영체제", "Silberschatz", "McGrawHill", "005.4", "기술과학"));
         books.add(new Book("철학의 이해", "김임시", "임시출판사", "100", "철학"));
         books.add(new Book("경영학 원론", "이임시", "임시출판사", "325", "경영"));
-        books.add(new Book("의학개론", "최임시", "임시출판사", "510", "의학"));
-        books.add(new Book("AI 개론", "홍임시", "임시출판사", "006", "컴퓨터"));
+        books.add(new Book("AI 개론", "홍임시", "임시출판사", "006", "기술과학"));
     }
 }
